ef869428449b773c3e04b80e4dca7dfc311f27e0
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2021-12-22 17:34:07 +0600

    login

--- todo-compete/front/angular-todo/src/app/login/login.component.ts
+++ todo-compete/front/angular-todo/src/app/login/login.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username = "something";
+  password = "";
+  errMessage = "Invalid credentials";
+  invalidLogin = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleLogin(){
+    console.log(this.username)
+    if(this.username === 'pks' && this.password ==='abc'){
+      this.invalidLogin = false;
+    }else{
+      this.invalidLogin = true;
+    }
   }
 
 }
