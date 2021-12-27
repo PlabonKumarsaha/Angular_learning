056fed435792c2c1c82819efcf008db808856f78
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2021-12-27 12:00:02 +0600

    params added

--- todo-compete/front/angular-todo/src/app/login/login.component.ts
+++ todo-compete/front/angular-todo/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,7 @@ export class LoginComponent implements OnInit {
   errMessage = "Invalid credentials";
   invalidLogin = false;
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
   }
@@ -20,6 +21,7 @@ export class LoginComponent implements OnInit {
     console.log(this.username)
     if(this.username === 'pks' && this.password ==='abc'){
       this.invalidLogin = false;
+      this.router.navigate(['welcome',this.username])
     }else{
       this.invalidLogin = true;
     }
