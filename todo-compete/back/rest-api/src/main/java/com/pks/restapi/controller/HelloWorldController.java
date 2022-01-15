69aaa020a45ed01a346130abfc72142c06b1ff6d
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2022-01-15 12:51:55 +0600

    backend added

---
+++ todo-compete/back/rest-api/src/main/java/com/pks/restapi/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.pks.restapi.controller;
+
+import com.pks.restapi.model.Response;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    String helloWorld(){
+        return "hello world";
+    }
+
+    @GetMapping("/hello/{path-var}")
+    Response helloWorld2(@PathVariable("path-var") String message){
+        String returnVal = "Hello from "+message.toString()+".";
+        return new Response(returnVal);
+    }
+}
---
+++ todo-compete/back/rest-api/src/main/java/com/pks/restapi/model/Response.java
@@ -0,0 +1,24 @@
+package com.pks.restapi.model;
+
+public class Response {
+    String message;
+
+    public Response(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Response{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
