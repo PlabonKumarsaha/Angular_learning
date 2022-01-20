ae63b2d6befcfe6a82c3d0defa67e1e689e26389
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2022-01-20 17:26:16 +0600

    service call

--- todo-compete/front/angular-todo/src/app/listtodo/listtodo.component.ts
+++ todo-compete/front/angular-todo/src/app/listtodo/listtodo.component.ts
@@ -1,29 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
-export class Todos{
-  constructor(
-    public id:number,
-    public description:string,
-    public done:boolean,
-    public targetDate: Date
-  ){
-
-  }
-}
-
 @Component({
   selector: 'app-listtodo',
   templateUrl: './listtodo.component.html',
   styleUrls: ['./listtodo.component.css']
 })
+
+export class Todo{
+  constructor(public id : number,
+    public description: string,
+    public done:boolean,
+    public targetDate : Date){
+
+  }
+}
 export class ListtodoComponent implements OnInit {
 
-  todos=[
-    new Todos(1,"Learn to dance",false,new Date()),
-    new Todos(2,"Learn Angular",true,new Date()),
-    new Todos(3,"Learn Django",false,new Date()),
-
-  ];
+  todos :Todo[]= [];
+  // todos=[
+  //   {"id":"1","description":"nachmu"},
+  //   {"id":"2","description":"gaimu"}
+  // ];
   // todo = {
   //   id: 1,
   //   description : 'This is something',
---
+++ todo-compete/front/angular-todo/src/app/service/data/todo-data.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Todo } from 'src/app/listtodo/listtodo.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoDataService {
+
+  constructor(
+    private http:HttpClient
+  ) { }
+  retriveAllTodos(username: string){
+    return this.http.get<Todo[]>(`http://localhost:8809/${username}/username/todos`)
+  }
+}
